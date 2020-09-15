8bfae6a4e80c8a7f4699c3e8f83abc0581e41acc
Michael Scherrer <michaelscherrer23@gmail.com>
2020-09-15 14:56:58 -0700

    added a clickable view

--- App.tsx
+++ App.tsx
@@ -1,11 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React,{useState} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+
+  const [count, setCount] = useState(0);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>you have clicked {count} times!</Text>
+
+      <View onClick={() => setCount(count + 1)} style={styles.touchArea}>
+        <Text>click here</Text>
+        </View>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -17,5 +25,14 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+
+  touchArea: {
+    marginTop: 10,
+    backgroundColor: '#defce7',
+    height: 100,
+    width: '50%',
+    textAlign: 'center',
+
   },
 });
